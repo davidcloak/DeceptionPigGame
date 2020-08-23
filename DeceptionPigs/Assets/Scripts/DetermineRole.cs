c516e1b10bffffb0d2874a462f13881c870e61b6
davidcloak <35105111+davidcloak@users.noreply.github.com>
2020-08-23 12:17:36 -0400

    Role for multple players
    * Roles are assigned to multple players
    - Still need fixing for UI but each player can have their own UI
    - A brige need made to connect player and UI stats still

---
+++ DeceptionPigs/Assets/Scripts/DetermineRole.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetermineRole : MonoBehaviour
+{
+    public float playersJoined = 6f;
+    GameObject[] players;
+
+    void Start()
+    {
+        players = new GameObject[(int) playersJoined];
+        
+        //gets the children of players
+        WithForLoop();
+
+        //Shuffles the array of players
+        ShuffleArray();
+
+        RolesDetermined();
+    }
+
+    void WithForLoop()
+    {
+        for (int i = 0; i < playersJoined; ++i)
+        {
+            players[i] = transform.GetChild(i).gameObject;
+        }
+    }
+
+    void ShuffleArray()
+    {
+        for (int i = players.Length - 1; i > 0; i--)
+        {
+            int randomIndex = Random.Range(0, i + 1);
+
+            GameObject temp = players[i];
+            players[i] = players[randomIndex];
+            players[randomIndex] = temp;
+        }
+    }
+
+    void RolesDetermined()
+    {
+        for (int i = 0; i < players.Length; i++)
+        {
+            if (i == 0 || i == 5)
+            {
+                print(players[i] + " you are wolf");
+                players[i].GetComponent<MeshRenderer>().material.SetColor("_Color", Color.red);
+            }
+            else
+            {
+                print(players[i]+" you are pig");
+                players[i].GetComponent<MeshRenderer>().material.SetColor("_Color", Color.blue);
+            }
+        }
+    }
+}
